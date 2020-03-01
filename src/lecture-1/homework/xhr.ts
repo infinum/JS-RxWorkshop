63c8337b548ec4d8d6c495ccce9f9bae782b8b52
Kristijan Bambir <kristijan.bambir@infinum.hr>
2020-03-01 22:35:32 +0100

    Implement XHR with observables

---
+++ src/lecture-1/homework/xhr.ts
@@ -0,0 +1,55 @@
+import { Observable } from 'rxjs';
+
+export interface IXHROptions {
+  body?: any;
+  headers?: {
+    [key: string]: string;
+  };
+}
+
+export interface IXHRResponse {
+  response: string;
+  responseText: string;
+  status: number;
+  statusText: string;
+}
+
+export function xhr(method: string, url: string, options?: IXHROptions): Observable<IXHRResponse> {
+  return new Observable((subscriber) => {
+    const request = new XMLHttpRequest();
+
+    request.onload = function() {
+      subscriber.next({
+        response: request.response,
+        responseText: request.responseText,
+        status: request.status,
+        statusText: request.statusText,
+      });
+    };
+
+    request.onloadend = function() {
+      subscriber.complete();
+    };
+
+    request.onerror = function() {
+      subscriber.error({
+        status: request.status,
+        statusText: request.statusText,
+      });
+    };
+
+    request.open(method, url);
+
+    if (options?.headers) {
+      Object.keys(options.headers).forEach((header) => {
+        request.setRequestHeader(header, options.headers[header]);
+      });
+    }
+
+    request.send(JSON.stringify(options?.body));
+
+    return () => {
+      request.abort();
+    };
+  });
+}
