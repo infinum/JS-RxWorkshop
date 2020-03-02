ea44fefbbc9bb830b0579f9841211ba0792da988
Kristijan Bambir <kristijan.bambir@infinum.hr>
2020-03-02 16:23:33 +0100

    Move interfaces to xhr module

--- src/lecture-1/homework/xhr.solution.ts
+++ src/lecture-1/homework/xhr.solution.ts
@@ -1,17 +1,5 @@
 import { Observable } from 'rxjs';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export interface XHROptions<TBody = any> {
-  body?: TBody;
-  headers?: Record<string, string>;
-}
-
-export interface XHRResponse {
-  response: string;
-  responseText: string;
-  status: number;
-  statusText: string;
-}
+import { XHROptions, XHRResponse } from './xhr';
 
 export function xhr(method: string, url: string, options?: XHROptions): Observable<XHRResponse> {
   return new Observable((subscriber) => {
--- src/lecture-1/homework/xhr.ts
+++ src/lecture-1/homework/xhr.ts
@@ -1,5 +1,17 @@
 import { Observable } from 'rxjs';
-import { XHROptions, XHRResponse } from './xhr.solution';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface XHROptions<TBody = any> {
+  body?: TBody;
+  headers?: Record<string, string>;
+}
+
+export interface XHRResponse {
+  response: string;
+  responseText: string;
+  status: number;
+  statusText: string;
+}
 
 export function xhr(method: string, url: string, options?: XHROptions): Observable<XHRResponse> {
   // TODO: implement!
