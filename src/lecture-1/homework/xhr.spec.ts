fb9534d1bbcf1caef5975299733fa5263593098c
Kristijan Bambir <kristijan.bambir@infinum.hr>
2020-03-02 10:35:38 +0100

    Add test to check if XHR is cancellable

--- src/lecture-1/homework/xhr.spec.ts
+++ src/lecture-1/homework/xhr.spec.ts
@@ -33,16 +33,6 @@ fdescribe('Lecture 1 - XHR', () => {
     expect(request.method).toBe('GET');
   });
 
-  it('should set request headers', (done: DoneFn) => {
-    xhr('GET', '/infinum/index', { headers: { header: 'value' } }).subscribe((response) => {
-      expect(response).toBeDefined();
-      done();
-    });
-
-    const request = jasmine.Ajax.requests.mostRecent();
-    expect(request.requestHeaders).toEqual({ header: 'value' });
-  });
-
   it('should make a POST request with a body', (done: DoneFn) => {
     xhr('POST', '/infinum/index', { body: { name: 'John' } }).subscribe((response) => {
       expect(response).toBeDefined();
@@ -54,18 +44,47 @@ fdescribe('Lecture 1 - XHR', () => {
     expect(Object.keys(request.data())[0]).toEqual(JSON.stringify({ name: 'John' }));
   });
 
+  it('should set request headers', (done: DoneFn) => {
+    xhr('GET', '/infinum/index', { headers: { header: 'value' } }).subscribe((response) => {
+      expect(response).toBeDefined();
+      done();
+    });
+
+    const request = jasmine.Ajax.requests.mostRecent();
+    expect(request.requestHeaders).toEqual({ header: 'value' });
+  });
+
   it('should handle errors', (done: DoneFn) => {
-    xhr('GET', '/infinum/error').subscribe({
-      error: (error) => {
-        expect(error.status).toBe(500);
-        done();
-      },
+    xhr('GET', '/infinum/error').subscribe(null, (error) => {
+      expect(error.status).toBe(500);
+      done();
     });
   });
 
   it('should handle timeouts', () => {
     const onErrorSpy = jasmine.createSpy('onError');
-    xhr('GET', '/infinum/timeout').subscribe(null, onErrorSpy);
+    xhr('GET', '/infinum/timeout').subscribe({ error: onErrorSpy });
     expect(onErrorSpy).toHaveBeenCalled();
+  });
+
+  it('should complete after response is received', () => {
+    const onCompleteSpy = jasmine.createSpy('onComplete');
+    xhr('GET', '/infinum/index').subscribe({ complete: onCompleteSpy });
+    expect(onCompleteSpy).toHaveBeenCalled();
+  });
+
+  it('should be cancellable', (done: DoneFn) => {
+    const onSuccessSpy = jasmine.createSpy('onSuccess');
+    const subscription = xhr('GET', '/infinum/long-request').subscribe(onSuccessSpy);
+    subscription.unsubscribe();
+
+    const request = jasmine.Ajax.requests.mostRecent();
+    setTimeout(() => {
+      request.respondWith({
+        status: 200,
+      });
+      expect(onSuccessSpy).not.toHaveBeenCalled();
+      done();
+    }, 0);
   });
 });
