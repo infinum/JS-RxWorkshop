a2ab63f94d76a5f3d5f690fe71df5b8d34045d36
Ivica Batinic <ivica.batinic@infinum.hr>
2020-03-20 00:06:57 +0100

    Finish task 1

--- src/lecture-1/task-1-from-event/from-event.ts
+++ src/lecture-1/task-1-from-event/from-event.ts
@@ -1,5 +1,11 @@
 import { Observable } from 'rxjs';
 
 export function fromEvent(element: HTMLElement, eventType: string): Observable<Event> {
-  return null;
+  return new Observable((observer) => {
+    const handler = (event: Event): void => observer.next(event);
+
+    element.addEventListener(eventType, handler);
+
+    return (): void => element.removeEventListener(name, handler);
+  });
 }
