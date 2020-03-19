2afc493ebcbedecd44c3c3d1f16bd8688ce0be2a
Ivica Batinic <ivica.batinic@infinum.hr>
2020-03-20 00:23:25 +0100

    Implement interval

--- src/lecture-1/task-3-interval/interval.ts
+++ src/lecture-1/task-3-interval/interval.ts
@@ -1,5 +1,11 @@
 import { Observable } from 'rxjs';
 
 export function interval(intervalMs: number): Observable<number> {
-  return null;
+  return new Observable((observer) => {
+    const interval = setInterval(() => {
+      observer.next();
+    }, intervalMs);
+
+    return (): void => clearInterval(interval);
+  });
 }
