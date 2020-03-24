a5d7e3863ee7495933d764e8125fe5ccfb4e0bdf
Ivana Leto <ivana.leto@infinum.hr>
2020-03-24 11:26:46 +0100

    Add task 4

--- src/lecture-1/task-4-req/req.ts
+++ src/lecture-1/task-4-req/req.ts
@@ -11,5 +11,43 @@ export interface IReqResponse {
 }
 
 export function req<TBody = void>(method: string, url: string, options?: IReqOptions<TBody>): Observable<IReqResponse> {
-  return null;
+  return new Observable<IReqResponse>((observer) => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', () => {
+      observer.next({
+        response: xhr.response,
+        status: xhr.status,
+      });
+      observer.complete();
+    });
+
+    xhr.addEventListener('error', () => {
+      observer.error({
+        response: null,
+        status: xhr.status,
+      });
+    });
+
+    xhr.open(method, url);
+
+    if (options && options.headers) {
+      Object.keys(options.headers).forEach((key) => {
+        xhr.setRequestHeader(key, options.headers[key]);
+      });
+    }
+
+    xhr.responseType = 'json';
+
+    if (method == 'POST' && options && options.body) {
+      let json = JSON.stringify(options.body);
+      xhr.send(json);
+    } else {
+      xhr.send();
+    }
+
+    return () => {
+      xhr.abort();
+    };
+  });
 }
