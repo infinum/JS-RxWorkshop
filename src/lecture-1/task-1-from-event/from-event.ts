06edf7b3d8e90f08f6f63753fa5fffde9f13c459
Ivana Leto <ivana.leto@infinum.hr>
2020-03-24 11:26:18 +0100

    Add task 1

--- src/lecture-1/task-1-from-event/from-event.ts
+++ src/lecture-1/task-1-from-event/from-event.ts
@@ -1,5 +1,12 @@
 import { Observable } from 'rxjs';
 
 export function fromEvent(element: HTMLElement, eventType: string): Observable<Event> {
-  return null;
+  return new Observable<Event>((observer) => {
+    const eventHandler = (event) => observer.next(event);
+    element.addEventListener(eventType, eventHandler);
+
+    return () => {
+      element.removeEventListener(eventType, eventHandler);
+    };
+  });
 }
