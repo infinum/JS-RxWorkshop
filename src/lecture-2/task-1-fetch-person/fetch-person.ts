37fcba89c74187367c323118e2b9b904d3440cb9
Darko Raguž <darko.raguz@infinum.hr>
2020-04-20 17:40:31 +0200

    Homework 2

--- src/lecture-2/task-1-fetch-person/fetch-person.ts
+++ src/lecture-2/task-1-fetch-person/fetch-person.ts
@@ -1,7 +1,14 @@
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Person } from '../models/person.model';
+import { ajax } from 'rxjs/ajax';
+import { map } from 'rxjs/operators';
+import { IPerson } from '../interfaces/person.interface';
 
 export function fetchPerson(id: number): Observable<Person> {
-  console.log(id);
-  return of(null);
+  const url = `https://swapi.co/api/people/${id}/`;
+  return ajax.getJSON(url).pipe(
+    map((res: IPerson) => {
+      return new Person(res.name, res.birth_year);
+    }),
+  );
 }
--- src/lecture-2/task-2-search/search.ts
+++ src/lecture-2/task-2-search/search.ts
@@ -1,9 +1,15 @@
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { IPerson } from '../interfaces/person.interface';
+import { ISearchResult } from '../interfaces/search-result.interface';
 
 export function searchPeople(query$: Observable<string>): Observable<Array<string>> {
+  const baseUrl = 'https://swapi.co/api/people/?search=';
   return query$.pipe(
-    /* do stuff */
-    map(() => ['Lorem ipsum']),
+    debounceTime(250),
+    distinctUntilChanged(),
+    switchMap((query: string) => ajax.getJSON(`${baseUrl}${query}`)),
+    map((result: ISearchResult) => result.results.map((person: IPerson) => person.name)),
   );
 }
